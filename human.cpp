46855e6fa90beef540a6be718f621363d5ae439a
savebirds <138513802+savebirds@users.noreply.github.com>
2023-07-04 23:34:57 +0530

    Add files via upload

---
+++ human.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+// class human                                   
+// {                                   
+// private:                                   
+                                       
+// public:                                   
+                                   
+// };
+
+int main()
+{
+    cout<<"Hello world...!";
+}
